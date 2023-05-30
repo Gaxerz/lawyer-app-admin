a3175304c9a7ab1bc4a9f004692b97d9ae957f2a
Muhamad Hamza Younas <hy106625@gmail.com>
2023-05-30 11:33:40 +0500

    Update axios

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://calm-ruby-slug-wig.cyclic.app',
-  baseURL: 'http://localhost:9000',
+  baseURL: 'https://calm-ruby-slug-wig.cyclic.app',
+  // baseURL: 'http://localhost:9000',
 });
 
 export default instance;
